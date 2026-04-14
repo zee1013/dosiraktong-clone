f208b5b0327db0b5e0a92ae069fd6fe6c785163f
zee1013 <dmswl1251@gmail.com>
2026-04-14 11:49:53 +0900

    헤더, 메인 비주얼 스와이프/js연결

---
+++ js/swiper.js
@@ -0,0 +1,15 @@
+window.addEventListener("load", ()=>{
+    const swVisual = new Swiper(".sw-visual", {
+        effect: "fade",
+        loop: true,
+    // 슬라이드의 모션 속도를 transition 맞춘다.
+    speed: 1500,
+    autoplay: {
+      delay: 2500,
+      // 사용자가 마우스 클릭 드래그로 이동하면
+      // 아래 구문이 없으면 autoplya 가 해제되므로
+      // 이것을 방지해 주기위한 처리
+      disableOnInteraction: false,
+    },
+    })
+})
