3d92944fa8f90351b2cc0c0f89365f12b476d8d1
zee1013 <dmswl1251@gmail.com>
2026-04-16 11:44:18 +0900

    헤더 반응형

--- js/header.js
+++ js/header.js
@@ -1,6 +1,8 @@
 window.addEventListener("load", () => {
   // 스크롤 될 때
   const header = document.querySelector(".header");
+  const mbt = document.querySelector(".mbt")
+  const navMb = document.querySelector(".nav-mb")
   let scy = 0; // 스크롤 y값
 
   // 2. class 적용 여부 결정
@@ -13,8 +15,51 @@ window.addEventListener("load", () => {
     scy = window.document.documentElement.scrollTop;
     if (scy > 0) {
       header.classList.add("active");
+      mbt.classList.add("active");
     }else{
         header.classList.remove("active")
+        mbt.classList.remove("active")
     }
   });
+  // 모바일 메뉴 클릭시
+  const htmlRoot = document.querySelector("html")
+  mbt.addEventListener("click" , ()=>{
+    // contains() : 현재 ani라는 클래스가 있는지 없는지 파악
+    const state = mbt.classList.contains("ani")
+    if(state){
+      mbt.classList.remove("ani")
+      navMb.classList.remove("active")
+      // 윈도우 스크롤바가 나타남
+      htmlRoot.classList.remove("active")
+      header.classList.remove("active")
+      mbt.classList.remove("active")
+    }else{
+      mbt.classList.add("ani")
+      navMb.classList.add("active")
+      // 윈도우 스크롤바 없어짐
+      htmlRoot.classList.add("active")
+      header.classList.add("active")
+      mbt.classList.add("active")
+    }
+  })
+  // 반응형 처리
+  // 모바일 메뉴 초기화
+  window.addEventListener("resize", ()=>{
+    let winW = window.innerWidth
+    // 모바일에서 pc 전환할 때
+    if(winW > 1024){
+      mbt.classList.remove("ani")
+      navMb.classList.remove("active")
+      htmlRoot.classList.remove("active")
+      if(scy > 0){
+        // 스크롤 된 상태에서 화면 리사이징을 하면 
+        header.classList.add("active")
+        mbt.classList.add("active")
+        
+      }else{
+        header.classList.remove("active")
+        mbt.classList.remove("active")
+      }
+    }
+  })
 });
